6a8a000d73336d618d28aefd90c83975125f694c
Andrew Smith <andrewcs149@gmail.com>
2024-12-24 16:07:56 -0500

    update

--- src/tank.rs
+++ src/tank.rs
@@ -105,64 +105,14 @@ pub fn set_unit_destination(
     cmds.trigger(pf_events::InitializeFlowFieldEv(units));
 }
 
-// fn move_unit(
-//     mut q_unit: Query<(&mut Transform, &mut ExternalImpulse, &Speed), With<pf_comps::Destination>>,
-//     mut q_flowfield: Query<&mut FlowField>, // Query mutable FlowField
-//     time: Res<Time>,
-//     mut cmds: Commands,
-// ) {
-//     let delta_time = time.delta_secs();
-
-//     for mut flowfield in q_flowfield.iter_mut() {
-//         let mut units_to_remove = Vec::new();
-
-//         for unit in flowfield.units.iter() {
-//             if let Ok((mut unit_transform, mut ext_impulse, speed)) = q_unit.get_mut(*unit) {
-//                 let cell_below = flowfield.get_cell_from_world_position(unit_transform.translation);
-
-//                 let raw_direction = Vec3::new(
-//                     cell_below.best_direction.vector().x as f32,
-//                     0.0,
-//                     cell_below.best_direction.vector().y as f32,
-//                 )
-//                 .normalize();
-
-//                 // Only update rotation and movement if there is a meaningful direction.
-//                 if raw_direction.length_squared() > 0.000001 {
-//                     let move_direction = raw_direction.normalize();
-
-//                     // Compute yaw assuming forward is along +Z axis.
-//                     let yaw = f32::atan2(-move_direction.x, -move_direction.z);
-
-//                     unit_transform.rotation = Quat::from_rotation_y(yaw);
-
-//                     // Apply movement
-//                     let movement_impulse = move_direction * speed.0 * delta_time;
-//                     ext_impulse.impulse += movement_impulse;
-//                 } else {
-//                     // Once the unit arrives at its destination, remove the Destination component
-//                     // and flowfield (if there are no more units associated with the flowfield)
-//                     units_to_remove.push(unit);
-//                     // flowfield.remove_unit(*unit, &mut cmds);
-//                 }
-//             }
-//         }
-
-//         for unit in units_to_remove {
-//             flowfield.remove_unit(*unit, &mut cmds);
-//         }
-//     }
-// }
-
 fn move_unit(
     mut q_unit: Query<(&mut Transform, &mut ExternalImpulse, &Speed), With<pf_comps::Destination>>,
-    mut q_flowfield: Query<&mut FlowField>,
+    q_flowfield: Query<&FlowField>,
     time: Res<Time>,
-    mut cmds: Commands,
 ) {
     let delta_time = time.delta_secs();
 
-    for mut flowfield in q_flowfield.iter_mut() {
+    for flowfield in q_flowfield.iter() {
         let mut units_to_remove = Vec::new();
 
         for &unit in &flowfield.units {
@@ -189,10 +139,6 @@ fn move_unit(
                     units_to_remove.push(unit);
                 }
             }
-        }
-
-        for unit in units_to_remove {
-            flowfield.remove_unit(unit, &mut cmds);
         }
     }
 }

b52e1d6d132c36e1254fc42efefeaf908345a3aa
andrew <andrewcs149@gmail.com>
2025-03-02 22:11:24 -0500

    update

--- src/cmd_center_ui.rs
+++ src/cmd_center_ui.rs
@@ -20,7 +20,7 @@ struct MiniMapCtr;
 struct IconsCtr;
 
 #[derive(Component)]
-struct BuildBtnsCtr;
+struct BuildColumnsCtr;
 
 fn command_center_ui(mut cmds: Commands, my_assets: Res<MyAssets>) {
     let root_ctr = (
@@ -73,19 +73,21 @@ fn command_center_ui(mut cmds: Commands, my_assets: Res<MyAssets>) {
         )
     };
 
-    let build_btns_ctr = (
-        BuildBtnsCtr,
+    let build_columns_ctr = (
+        BuildColumnsCtr,
+        BackgroundColor(Color::srgb(0.12, 0.12, 0.12)),
         Node {
             width: Val::Percent(100.0),
             height: Val::Percent(60.0),
+            padding: UiRect::top(Val::Px(5.0)),
+            margin: UiRect::new(Val::Px(5.0), Val::Px(5.0), Val::ZERO, Val::ZERO),
             ..default()
         },
-        Name::new("Build Btns Ctr"),
+        Name::new("Build Columns Ctr"),
     );
 
-    let build_column = |margin_l: f32, margin_r: f32| -> (BackgroundColor, Node, Name) {
+    let build_column = |margin_l: f32, margin_r: f32| -> (Node, Name) {
         (
-            BackgroundColor(Color::srgb(0.12, 0.12, 0.12)),
             Node {
                 flex_direction: FlexDirection::Column,
                 width: Val::Percent(100.0),
@@ -97,16 +99,63 @@ fn command_center_ui(mut cmds: Commands, my_assets: Res<MyAssets>) {
         )
     };
 
+    let opt_ctr = || -> (BackgroundColor, BorderColor, Node, Name) {
+        (
+            BackgroundColor(Color::srgb(0.07, 0.07, 0.07)),
+            BorderColor(Color::srgb(0.8, 0.8, 0.8)),
+            Node {
+                height: Val::Percent(20.0),
+                margin: UiRect::bottom(Val::Px(5.0)),
+                border: UiRect::all(Val::Px(2.5)),
+                ..default()
+            },
+            Name::new("Build Option"),
+        )
+    };
+
+    let build_opt = |txt: &str| -> (Node, Text, TextFont, TextLayout, Name) {
+        (
+            Node {
+                margin: UiRect::all(Val::Auto),
+                ..default()
+            },
+            Text::new(txt),
+            TextFont {
+                font_size: 15.0,
+                ..default()
+            },
+            TextLayout::new_with_justify(JustifyText::Center),
+            Name::new("Build Option Ctr"),
+        )
+    };
+
     cmds.spawn(root_ctr).with_children(|parent| {
         parent.spawn(mini_map_ctr);
+
         parent.spawn(icons_ctr).with_children(|parent| {
             parent.spawn(icon(my_assets.cmd_cntr_structures.clone()));
             parent.spawn(icon(my_assets.cmd_cntr_units.clone()));
         });
 
-        parent.spawn(build_btns_ctr).with_children(|parent| {
-            parent.spawn(build_column(5.0, 2.5));
-            parent.spawn(build_column(2.5, 5.0));
+        parent.spawn(build_columns_ctr).with_children(|parent| {
+            parent
+                .spawn(build_column(5.0, 2.5))
+                .with_children(|parent| {
+                    parent.spawn(opt_ctr()).with_child(build_opt("Bldg 1"));
+                    parent.spawn(opt_ctr()).with_child(build_opt("Bldg 2"));
+                    parent.spawn(opt_ctr()).with_child(build_opt("Bldg 3"));
+                    parent.spawn(opt_ctr()).with_child(build_opt("Bldg 4"));
+                    parent.spawn(opt_ctr()).with_child(build_opt("Bldg 5"));
+                });
+            parent
+                .spawn(build_column(2.5, 5.0))
+                .with_children(|parent| {
+                    parent.spawn(opt_ctr()).with_child(build_opt("Unit 1"));
+                    parent.spawn(opt_ctr()).with_child(build_opt("Unit 2"));
+                    parent.spawn(opt_ctr()).with_child(build_opt("Unit 3"));
+                    parent.spawn(opt_ctr()).with_child(build_opt("Unit 4"));
+                    parent.spawn(opt_ctr()).with_child(build_opt("Unit 5"));
+                });
         });
     });
 }

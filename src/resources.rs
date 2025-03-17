d853fc2d10f878a612f2a07a200e4e1b8899f7ca
andrew <andrewcs149@gmail.com>
2025-03-17 09:19:12 -0400

    update

--- src/resources.rs
+++ src/resources.rs
@@ -1,4 +1,10 @@
-use bevy::prelude::*;
+use bevy::{
+    image::{
+        ImageAddressMode, ImageFilterMode, ImageLoaderSettings, ImageSampler,
+        ImageSamplerDescriptor,
+    },
+    prelude::*,
+};
 
 use crate::*;
 
@@ -39,12 +45,21 @@ impl DbgOptions {
 pub struct MyAssets {
     pub models: Models,
     pub images: Images,
+    pub textures: Textures,
     pub audio: Audio,
 }
 
 #[derive(Default)]
 pub struct Audio {
     pub place_structure: Handle<AudioSource>,
+}
+
+#[derive(Default)]
+pub struct Textures {
+    pub grass_clr: Handle<Image>,
+    pub grass_normal: Handle<Image>,
+    pub grass_roughness: Handle<Image>,
+    pub grass_occlusion: Handle<Image>,
 }
 
 #[derive(Default)]
@@ -178,6 +193,61 @@ pub struct GameCommands {
 }
 
 fn add_assets(mut my_assets: ResMut<MyAssets>, assets: Res<AssetServer>) {
+    // textures
+    my_assets.textures.grass_clr =
+        assets.load_with_settings("textures/grass/color.png", |s: &mut _| {
+            *s = ImageLoaderSettings {
+                sampler: ImageSampler::Descriptor(ImageSamplerDescriptor {
+                    address_mode_u: ImageAddressMode::Repeat,
+                    address_mode_v: ImageAddressMode::Repeat,
+                    ..default()
+                }),
+                ..default()
+            }
+        });
+
+    my_assets.textures.grass_normal =
+        assets.load_with_settings("textures/grass/normal_gl.png", |s: &mut _| {
+            *s = ImageLoaderSettings {
+                sampler: ImageSampler::Descriptor(ImageSamplerDescriptor {
+                    mag_filter: ImageFilterMode::Linear,
+                    min_filter: ImageFilterMode::Linear,
+                    address_mode_u: ImageAddressMode::Repeat,
+                    address_mode_v: ImageAddressMode::Repeat,
+                    ..default()
+                }),
+                ..default()
+            }
+        });
+
+    my_assets.textures.grass_roughness =
+        assets.load_with_settings("textures/grass/roughness.png", |s: &mut _| {
+            *s = ImageLoaderSettings {
+                sampler: ImageSampler::Descriptor(ImageSamplerDescriptor {
+                    mag_filter: ImageFilterMode::Linear,
+                    min_filter: ImageFilterMode::Linear,
+                    address_mode_u: ImageAddressMode::Repeat,
+                    address_mode_v: ImageAddressMode::Repeat,
+                    ..default()
+                }),
+                ..default()
+            }
+        });
+
+    my_assets.textures.grass_occlusion =
+        assets.load_with_settings("textures/grass/ambient_occlusion.png", |s: &mut _| {
+            *s = ImageLoaderSettings {
+                sampler: ImageSampler::Descriptor(ImageSamplerDescriptor {
+                    mag_filter: ImageFilterMode::Linear,
+                    min_filter: ImageFilterMode::Linear,
+                    address_mode_u: ImageAddressMode::Repeat,
+                    address_mode_v: ImageAddressMode::Repeat,
+                    ..default()
+                }),
+                ..default()
+            }
+        });
+
     // images
     my_assets.images.select_border = assets.load("imgs/select_border.png");
     my_assets.images.cursor_relocate = assets.load("imgs/cursor/relocate.png");

5118e66968f492b6070289491c4a79e19556a6f3
andrew <andrewcs149@gmail.com>
2025-03-18 08:12:30 -0400

    update

--- src/map.rs
+++ src/map.rs
@@ -5,7 +5,6 @@ use bevy::time::common_conditions::once_after_delay;
 use bevy_rts_camera::Ground;
 use bevy_rts_pathfinding::components as pf_comps;
 use bevy_rts_pathfinding::grid::Grid;
-use std::f32::INFINITY;
 use std::time::Duration;
 
 use crate::resources::MyAssets;
@@ -18,15 +17,21 @@ impl Plugin for MapPlugin {
     fn build(&self, app: &mut App) {
         app.add_systems(
             Startup,
-            (spawn_ground, spawn_light, spawn_obstacle, spawn_grid).chain(),
-        );
-        app.add_systems(
-            Update,
             (
-                spawn_obstacle_2.run_if(once_after_delay(Duration::from_secs_f32(4.0))),
-                // despawn_obstacles.run_if(once_after_delay(Duration::from_secs(6))),
-            ),
+                spawn_ground,
+                spawn_light,
+                // spawn_obstacle,
+                spawn_grid,
+            )
+                .chain(),
         );
+        // app.add_systems(
+        //     Update,
+        //     (
+        // spawn_obstacle_2.run_if(once_after_delay(Duration::from_secs_f32(4.0))),
+        // despawn_obstacles.run_if(once_after_delay(Duration::from_secs(6))),
+        //     ),
+        // );
     }
 }
 
@@ -41,39 +46,57 @@ fn spawn_ground(
     mut materials: ResMut<Assets<StandardMaterial>>,
     my_assets: Res<MyAssets>,
 ) {
-    let desired_tile_size = 30.0;
-    let tile_factor = MAP_WIDTH / desired_tile_size;
+    // let desired_tile_size = 30.0;
+    // let tile_factor = MAP_WIDTH / desired_tile_size;
 
-    // Build the mesh from the builder.
-    let mut mesh = Plane3d::default().mesh().size(MAP_WIDTH, MAP_DEPTH).build();
+    // // Build the mesh from the builder.
+    // let mut mesh = Plane3d::default().mesh().size(MAP_WIDTH, MAP_DEPTH).build();
 
-    // Update the UVs so that the texture repeats.
-    if let Some(VertexAttributeValues::Float32x2(uvs)) = mesh.attribute(Mesh::ATTRIBUTE_UV_0) {
-        let new_uvs: Vec<[f32; 2]> = uvs
-            .iter()
-            .map(|uv| [uv[0] * tile_factor, uv[1] * tile_factor])
-            .collect();
+    // // Update the UVs so that the texture repeats.
+    // if let Some(VertexAttributeValues::Float32x2(uvs)) = mesh.attribute(Mesh::ATTRIBUTE_UV_0) {
+    //     let new_uvs: Vec<[f32; 2]> = uvs
+    //         .iter()
+    //         .map(|uv| [uv[0] * tile_factor, uv[1] * tile_factor])
+    //         .collect();
 
-        if let Some(m) = mesh.attribute_mut(Mesh::ATTRIBUTE_UV_0) {
-            *m = VertexAttributeValues::Float32x2(new_uvs);
-        }
-    }
+    //     if let Some(m) = mesh.attribute_mut(Mesh::ATTRIBUTE_UV_0) {
+    //         *m = VertexAttributeValues::Float32x2(new_uvs);
+    //     }
+    // }
 
-    let plane_handle = meshes.add(mesh);
+    // let plane_handle = meshes.add(mesh);
 
-    let material = StandardMaterial {
-        base_color_texture: Some(my_assets.textures.grass_clr.clone()),
-        normal_map_texture: Some(my_assets.textures.grass_normal.clone()),
-        // metallic_roughness_texture: Some(my_assets.textures.grass_roughness.clone()), // super shiny
-        perceptual_roughness: 1.0,
-        occlusion_texture: Some(my_assets.textures.grass_occlusion.clone()),
-        depth_bias: INFINITY,
-        ..Default::default()
-    };
+    // let material = StandardMaterial {
+    //     base_color_texture: Some(my_assets.textures.grass_clr.clone()),
+    //     normal_map_texture: Some(my_assets.textures.grass_normal.clone()),
+    //     // metallic_roughness_texture: Some(my_assets.textures.grass_roughness.clone()), // super shiny
+    //     perceptual_roughness: 1.0,
+    //     occlusion_texture: Some(my_assets.textures.grass_occlusion.clone()),
+    //     depth_bias: INFINITY,
+    //     ..Default::default()
+    // };
 
+    // cmds.spawn((
+    //     Mesh3d(plane_handle),
+    //     MeshMaterial3d(materials.add(material)),
+    //     Collider::cuboid(MAP_WIDTH / 2.0, 0.0, MAP_DEPTH / 2.0),
+    //     Sensor,
+    //     Ground,
+    //     pf_comps::MapBase,
+    //     Name::new("Map Base"),
+    // ));
+
+    // Ground
+    // rgb(95, 123, 155)
+    // rgb(111, 190, 111)
     cmds.spawn((
-        Mesh3d(plane_handle),
-        MeshMaterial3d(materials.add(material)),
+        Mesh3d(meshes.add(Plane3d::default().mesh().size(MAP_WIDTH, MAP_DEPTH))),
+        MeshMaterial3d(materials.add(StandardMaterial {
+            // base_color: Color::srgb(0.37, 0.48, 0.61), // blue
+            base_color: Color::srgb(0.44, 0.75, 0.44), // green
+            perceptual_roughness: 1.0,
+            ..Default::default()
+        })),
         Collider::cuboid(MAP_WIDTH / 2.0, 0.0, MAP_DEPTH / 2.0),
         Sensor,
         Ground,
